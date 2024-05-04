94443b5ace6c2f85b5f7096632847dabcc866cbf
c5346650 <manjun.jiao@sap.com>
2024-05-04 12:50:55 +0200

    add eic validation helper scripts/defs

---
+++ edge-integration-cell/external-redis/wait_for_rec_running_state.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the name of the RedisEnterpriseCluster
+cluster_name="rec"
+
+# Loop until state equals "Running"
+while true; do
+    state=$(oc get RedisEnterpriseCluster "$cluster_name" -o json | jq -r '.status.state')
+
+    if [[ "$state" == "Running" ]]; then
+        echo "State is Running. Exiting loop."
+        break
+    else
+        echo "State is $state. Waiting..."
+        sleep 10  # Adjust the sleep time as needed
+    fi
+done
