507200164930c698d5a8ac28221bbfc1bb1da512
kksat <22549266+kksat@users.noreply.github.com>
2024-08-09 13:06:18 +0200

    add makefile target to check if domain zone exists
    additional check before ARO deployment
    
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

---
+++ hack/domain-zone-exists.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+NAME=$(az network dns zone list --query "[?name=='${ARO_DOMAIN}'].name" -o tsv)
+if [ "${NAME}"=="${ARO_DOMAIN}" ]; then
+  echo "Domain zone ${ARO_DOMAIN} exists"
+else
+  echo "Domain zone ${ARO_DOMAIN} does not exist"
+  exit 1
+fi
