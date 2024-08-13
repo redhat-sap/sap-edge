2085b8fa02cc5fefb9180c8fac83ba05564df91b
kksat <22549266+kksat@users.noreply.github.com>
2024-08-13 13:13:29 +0200

    add license notice
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- hack/domain-zone-exists.sh
+++ hack/domain-zone-exists.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/env bash
 
+# SPDX-FileCopyrightText: 2024 SAP edge team
+# SPDX-FileContributor: Kirill Satarin (@kksat)
+# SPDX-FileContributor: Manjun Jiao (@mjiao)
+#
+# SPDX-License-Identifier: Apache-2.0
+
 NAME=$(az network dns zone list --query "[?name=='${ARO_DOMAIN}'].name" -o tsv)
 if [ "${NAME}" == "${ARO_DOMAIN}" ]; then
   echo "Domain zone ${ARO_DOMAIN} exists"
