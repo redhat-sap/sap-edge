9f839ad9d0603f1f57f8dbbd3d1f69b6f637db36
kksat <22549266+kksat@users.noreply.github.com>
2024-08-13 13:12:34 +0200

    fix shellcheck errors
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- hack/domain-zone-exists.sh
+++ hack/domain-zone-exists.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 NAME=$(az network dns zone list --query "[?name=='${ARO_DOMAIN}'].name" -o tsv)
-if [ "${NAME}"=="${ARO_DOMAIN}" ]; then
+if [ "${NAME}" == "${ARO_DOMAIN}" ]; then
   echo "Domain zone ${ARO_DOMAIN} exists"
 else
   echo "Domain zone ${ARO_DOMAIN} does not exist"
