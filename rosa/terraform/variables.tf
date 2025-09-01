8f5fa2ee81d0cdf4fdeaf0a974c6c2016437d9ef
kksat <22549266+kksat@users.noreply.github.com>
2025-09-01 14:00:24 +0200

    move default values for terraform variables to variables file
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>
    
    Co-authored-by: RishabhKodes <rishabhbhandari6@gmail.com>
    Co-authored-by: kksat <22549266+kksat@users.noreply.github.com>

--- rosa/terraform/variables.tf
+++ rosa/terraform/variables.tf
@@ -6,6 +6,11 @@ variable "aws_region" {
   type        = string
 }
 
+variable "cluster_name" {
+  description = "Name of the ROSA cluster"
+  type        = string
+}
+
 variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
@@ -14,39 +19,47 @@ variable "vpc_name" {
 variable "environment_tag" {
   description = "Environment tag value"
   type        = string
+  default     = "rosa"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_1_cidr" {
   description = "CIDR block for public subnet 1"
   type        = string
+  default     = "10.0.0.0/24"
 }
 
 variable "public_subnet_2_cidr" {
   description = "CIDR block for public subnet 2"
   type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "private_subnet_1_cidr" {
   description = "CIDR block for private subnet 1"
   type        = string
+  default     = "10.0.2.0/24"
 }
 
 variable "private_subnet_2_cidr" {
   description = "CIDR block for private subnet 2"
   type        = string
+  default     = "10.0.3.0/24"
 }
 
 variable "public_subnet_3_cidr" {
   description = "CIDR block for public subnet 3"
   type        = string
+  default     = "10.0.4.0/24"
 }
 
 variable "private_subnet_3_cidr" {
   description = "CIDR block for private subnet 3"
   type        = string
+  default     = "10.0.5.0/24"
 }
