662b413383d4a9b4fd623ba376fd12e3897b661d
kksat <22549266+kksat@users.noreply.github.com>
2025-09-03 09:54:26 +0200

    fix tflint error
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- rosa/terraform/variables.tf
+++ rosa/terraform/variables.tf
@@ -6,11 +6,6 @@ variable "aws_region" {
   type        = string
 }
 
-variable "cluster_name" {
-  description = "Name of the ROSA cluster"
-  type        = string
-}
-
 variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
